1fa7b27b9218959ce6d3d713b26c4a5c1145e475
barubarubbq86-coder <barubarubbq86@gmail.com>
2026-09-20 20:41:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fictional-map-maker-pwa-v22';
+const CACHE='fictional-map-maker-pwa-v24';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
